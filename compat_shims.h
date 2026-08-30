397a3b4cb91ad94267fe0ec201a40f1b204b0178
Francesco Palazzo <francesco@palazzo.me>
2026-08-30 15:47:32 +0200

    Some more GX typedefs

--- compat_shims.h
+++ compat_shims.h
@@ -1,10 +1,31 @@
 #pragma once
 #include <limits>
 #define __fabsf fabsf
+#define __fabs fabs
 typedef __builtin_va_list __va_list;
 extern "C" int __float_max[];
+
+#include <cstring>
 
 // aurora fixes
 #include <dolphin/gx/GXTexture.h>
 typedef GXTexObj _GXTexObj;
 typedef GXTlutObj _GXTlutObj;
+typedef GXRenderModeObj _GXRenderModeObj;
+typedef GXTexGenType _GXTexGenType;
+typedef GXTexGenSrc _GXTexGenSrc;
+typedef GXTevOp _GXTevOp;
+typedef GXTevBias _GXTevBias;
+typedef GXTevScale _GXTevScale;
+typedef GXTevAlphaArg _GXTevAlphaArg;
+typedef GXTevRegID _GXTevRegID;
+typedef GXCullMode _GXCullMode;
+typedef GXBlendFactor _GXBlendFactor;
+typedef GXCompare _GXCompare;
+typedef GXLightID _GXLightID;
+typedef GXTexMtxType _GXTexMtxType;
+typedef GXVtxFmt _GXVtxFmt;
+typedef GXProjectionType _GXProjectionType;
+typedef GXTexFmt _GXTexFmt;
+typedef GXPrimitive _GXPrimitive;
+typedef GXTlut _GXTlut;
