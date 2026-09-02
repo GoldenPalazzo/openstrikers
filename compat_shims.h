0f296dcc9ea87ea79397189ecea0b524f883a7f3
Francesco Palazzo <francesco@palazzo.me>
2026-09-02 14:08:07 +0200

    Some other stubs

--- compat_shims.h
+++ compat_shims.h
@@ -34,6 +34,9 @@ inline double __frsqrte(double x) {
 
 // aurora fixes
 #include <dolphin/gx/GXTexture.h>
+#include <dolphin/gx/GXManage.h>
+#include <dolphin/gx/GXFrameBuffer.h>
+#include <dolphin/vi.h>
 typedef GXTexObj _GXTexObj;
 typedef GXTlutObj _GXTlutObj;
 typedef GXRenderModeObj _GXRenderModeObj;
@@ -59,7 +62,79 @@ inline void GXSetGPMetric(int, int) {}
 inline void GXReadGPMetric(u32* val0, u32* val1) { *val0 = 0; *val1 = 0; }
 inline void GXInitFogAdjTable(void* table, u16 width, float projParam[4][4]) { /* stub */ }
 inline GXRenderModeObj GXEurgb60Hz480IntDf = {}; // stub
-extern "C" inline void GXWaitDrawDone() {}
 typedef const float (*CMtxP)[4];
 inline void SISetSamplingRate(int) {}
 inline void PADSetSamplingCallback(void (*)(void)) {}  // PADSamplingCallback = void (*)(void)
+
+#include <dolphin/mtx.h>
+#include <dolphin/mtx/mtx44ext.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+inline void GXWaitDrawDone() {}
+// --- Cache / PPC stubs ---
+inline void PPCSync() {}
+inline void DCFlushRangeNoSync(void*, u32) {}
+inline void DCStoreRangeNoSync(void*, u32) {}
+inline void DCStoreRange(void*, u32) {}
+
+inline void DCFlushRange(void*, u32) {}
+inline void DCZeroRange(void*, u32) {}
+inline void DCInvalidateRange(void*, u32) {}
+
+
+// --- VI stubs ---
+
+inline void VISetBlack(int) {}
+inline void VIWaitForRetrace() {}
+
+inline u32 VIGetRetraceCount() { return 0; }
+inline void VISetNextFrameBuffer(void*) {}
+inline VIRetraceCallback VISetPreRetraceCallback(VIRetraceCallback) {}
+inline VIRetraceCallback VISetPostRetraceCallback(VIRetraceCallback cb);
+inline u32 VIGetDTVStatus() { return 0; }
+
+
+
+// --- OS stubs ---
+
+inline void OSYieldThread() {}
+inline u32 OSGetConsoleType() { return 0; }
+
+inline BOOL OSDisableInterrupts() { return 0; }
+inline BOOL OSRestoreInterrupts(BOOL) {return 0;}
+inline u32 OSGetResetCode() { return 0; }
+inline void OSResetSystem(int, u32, BOOL) {}
+inline u32 OSGetSoundMode() { return 0; }
+
+inline void OSSetSoundMode(u32) {}
+inline u8 OSGetLanguage() { return 0; }
+inline u32 OSGetProgressiveMode() { return 0; }
+
+inline void OSSetProgressiveMode(u32) {}
+inline u32 OSGetEuRgb60Mode() { return 0; }
+inline void OSSetEuRgb60Mode(u32) {}
+
+inline BOOL OSGetResetButtonState() { return 0; }
+inline void OSClearStack(u8) {}
+
+// --- GX stubs ---
+inline void GXPeekARGB(u16, u16, u32* val) { *val = 0; }
+
+inline void GXPokeColorUpdate(GXBool) {}
+inline void GXPokeBlendMode(GXBlendMode, GXBlendFactor, GXBlendFactor, GXLogicOp) {}
+
+inline void GXPokeARGB(u16, u16, u32) {}
+inline f32 GXGetYScaleFactor(u16, u16) { return 1.0f; }
+extern GXRenderModeObj GXNtsc480Prog; // declaration only, defined in compat_shims.cpp
+
+// --- VM stubs ---
+
+inline void VMInit(uintptr_t baseAddr, size_t initialCommitSize, uintptr_t limitAddr) {}
+inline void VMAlloc(uintptr_t address, size_t size) {}
+typedef void (*VMLogStatsCallback)(u32 faultAddr, u32 mainAddr, u32 pageIndex, u32 elapsed, u32 wroteBack);
+inline void VMSetLogStatsCallback(VMLogStatsCallback cb) {}
+#ifdef __cplusplus
+}
+#endif
