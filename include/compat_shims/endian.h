c0fcd78950d76349df031fc48f95f48d7928db94
Francesco Palazzo <francesco@palazzo.me>
2026-09-06 23:32:26 +0200

    endianness swap utility

---
+++ include/compat_shims/endian.h
@@ -0,0 +1,20 @@
+// compat_shims/endian.h
+#ifdef __cplusplus
+#pragma once
+#include <cstdint>
+#include <type_traits>
+
+template <typename T>
+constexpr T bswap(T val) noexcept
+{
+    static_assert(std::is_unsigned_v<T>, "nlByteSwap requires an unsigned type");
+    if constexpr (sizeof(T) == 2)
+        return __builtin_bswap16(val);
+    else if constexpr (sizeof(T) == 4)
+        return __builtin_bswap32(val);
+    else if constexpr (sizeof(T) == 8)
+        return __builtin_bswap64(val);
+    else
+        static_assert(sizeof(T) == 0, "unsupported dimension");
+}
+#endif
